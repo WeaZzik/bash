47c3c43797e19f62f1acf3b8ae41393c221de2e0
Luka <57722947+WeaZzik@users.noreply.github.com>
2023-10-18 11:32:42 +0200

    Update lamp.sh

--- lamp.sh
+++ lamp.sh
@@ -43,7 +43,7 @@ then
   yes | apt install ca-certificates apt-transport-https software-properties-common curl lsb-release
   yes | curl -sSL https://packages.sury.org/php/README.txt | sudo bash -x
   yes | apt-get update
-yes | apt install php8.1 libapache2-mod-php8.1 php8.1-fpm php8.1-cli php8.1-common php8.1-curl php8.1-bcmath php8.1-intl php8.1-mbstring php8.1-xmlrpc php8.1-mcrypt php8.1-mysql php8.1-gd php8.1-xml php8.1-cli php8.1-zip php8.1-mysqli
+yes | apt install php8.1 libapache2-mod-php8.1 php8.1-fpm php8.1-cli php8.1-common php8.1-bz2 php8.1-curl php8.1-bcmath php8.1-intl php8.1-mbstring php8.1-xmlrpc php8.1-mcrypt php8.1-mysql php8.1-gd php8.1-xml php8.1-cli php8.1-zip php8.1-mysqli
 fi
 
 if [ "$mariadb_choice" = "" ] || [ "$mariadb_choice" = "Y" ] || [ "$mariadb_choice" = "y" ]
